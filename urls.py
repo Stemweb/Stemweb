d56de4b34ff7a36f0d5c6fc36824511071cda459
Assamite <slinkola@cs.helsinki.fi>
2012-02-13 13:36:53 +0200

    New layout prototype when running server localhost/home. Adds fixed menubar on top of site. Needs stemweb.css in your local media path

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 import settings
-#from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     url(r'^server_error', 'django_proto.views.server_error'),
     url(r'^script_failure', 'django_proto.views.script_failure'),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT}),
+    url(r'^layout/', direct_to_template, {'template' : 'layout_proto.html'}),
     
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
