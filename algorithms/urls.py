abe7c7ccf43ef2fd5be24d48420f2c503a62e6ff
Assamite <slinkola@cs.helsinki.fi>
2012-11-22 23:37:20 +0200

    One file

--- algorithms/urls.py
+++ algorithms/urls.py
@@ -5,7 +5,7 @@ from views import base, details, delete_runs, run, results
 from .settings import URL_PREFIX
 
 urlpatterns = patterns('',
-    url(r'^%s/base/$' % URL_PREFIX, base, name = 'algorithms_base_url'),
+    url(r'^%s/base/' % URL_PREFIX, base, name = 'algorithms_base_url'),
     url(r'^%s/(?P<algo_id>\d+)/$' % URL_PREFIX, details, name = 'algorithms_details_url'),
     url(r'^%s/delete/$' % URL_PREFIX, delete_runs, name = 'algorithms_delete_runs_url'),
     url(r'^%s/run/(?P<algo_id>\d+)/$' % URL_PREFIX, run, name = 'algorithms_run_algorithm_url'),
