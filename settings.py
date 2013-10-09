ed0c20d8d52e4323cdd978fc989482bfe119829d
Assamite <slinkola@cs.helsinki.fi>
2013-10-09 18:27:35 +0300

    Another two line fix for new wsgi settings to work on users-server. (this is kind of ridiculous as I cannot really check what goes wrong locally.)

--- settings.py
+++ settings.py
@@ -121,7 +121,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = lstrings.root_urls
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-	"django.core.context_processors.auth",
+	"django.contrib.context_processors.auth",
 	"django.core.context_processors.debug",
 	"django.core.context_processors.i18n",
 	"django.core.context_processors.media",
