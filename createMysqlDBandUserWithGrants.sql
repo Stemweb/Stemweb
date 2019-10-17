51fb40834bafd59fd7e5e3f9e3c4d46aa2a4ee2a
peter fugger <peter.fugger@univie.ac.at>
2019-10-17 16:56:48 +0200

    use mysql-DB

---
+++ createMysqlDBandUserWithGrants.sql
@@ -0,0 +1,10 @@
+export MY_USER=stemweb
+export MY_PASSW=ChangeMe
+export MY_HOST=localhost
+export MY_DB=stemwebdb_v1
+mysql -u root -e "CREATE USER '$MY_USER'@'$MY_HOST' IDENTIFIED BY '$MY_PASSW'"
+mysql -u root -e "CREATE DATABASE $MY_DB"
+mysql -u root -e "GRANT ALL ON *.* TO '$MY_USER'@'$MY_HOST'"
+mysql -u root -e "GRANT USAGE ON *.* TO '$MY_USER'@'$MY_HOST'"
+mysql -u root -e "FLUSH PRIVILEGES"
+mysql -D $MY_DB -u $MY_USER -p$MY_PASSW -e "SET FOREIGN_KEY_CHECKS = 0"
