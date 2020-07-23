5cb06dbc8bcbc28f3c6151fb57470b490d1920cb
peter fugger <peter.fugger@univie.ac.at>
2020-07-23 15:39:59 +0200

    re-add registration

---
+++ Stemweb/third_party_apps/registration/backends/default/urls.py
@@ -0,0 +1,38 @@
+"""
+URLconf for registration and activation, using django-registration's
+one-step backend.
+
+If the default behavior of these views is acceptable to you, simply
+use a line like this in your root URLconf to set up the default URLs
+for registration::
+
+    (r'^accounts/', include('registration.backends.simple.urls')),
+
+This will also automatically set up the views in
+``django.contrib.auth`` at sensible default locations.
+
+If you'd like to customize the behavior (e.g., by passing extra
+arguments to the various views) or split up the URLs, feel free to set
+up your own URL patterns for these views instead.
+
+"""
+
+
+from django.conf.urls import url, include
+from django.views.generic import TemplateView
+
+from Stemweb.third_party_apps.registration.views import activate
+from Stemweb.third_party_apps.registration.views import register
+
+
+urlpatterns = [
+               url(r'^register/$',
+                   register,
+                   {'backend': 'Stemweb.third_party_apps.registration.backends.default.DefaultBackend'},
+                   name='registration_register'),
+               url(r'^register/closed/$',
+                   TemplateView.as_view(
+                       template_name="registration_closed.html"),
+                   name='registration_disallowed'),
+               url(r'', include('Stemweb.third_party_apps.registration.auth_urls')),
+               ]
