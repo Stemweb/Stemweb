f0fad184b57a4cad29ba2c29327f4a68496802c1
peter fugger <peter.fugger@univie.ac.at>
2020-11-25 09:58:21 +0100

    actualised documentation and re-deactivated csrf (as workaround)

--- Stemweb/settings.py
+++ Stemweb/settings.py
@@ -102,7 +102,7 @@ SECRET_KEY = ls.secret_key
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+#    'django.middleware.csrf.CsrfViewMiddleware',                # disabled as workaround
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'Stemweb.third_party_apps.pagination.middleware.PaginationMiddleware',
